8f59949d468370cbd5cfad326735a5ead1319422
marrip <martin.rippin@scilifelab.uu.se>
2021-06-21 15:51:48 +0200

    Feat: Add plotting R script

---
+++ scripts/bm_plot.R
@@ -0,0 +1,37 @@
+# Load/install libraries
+
+packages <- c('ggplot2', 'tidyr')
+for(package in packages){
+  if(!require(package, character.only = T)){
+    install.packages(package)
+  }
+}
+
+# Functions
+
+get_data <- function(input){
+  bm_data <- data.frame()
+  for(bm in input){
+    data <- read.delim(file = bm, sep = "\t")
+    data$file <- bm
+    bm_data <- rbind(bm_data, data)
+  }
+  bm_data <- separate(bm_data, "file", c("folder", "unit", "tool", "sample"), sep="/")
+  bm_data$sample <- gsub(".tsv", "", bm_data$sample)
+  bm_data <- separate(bm_data, "sample", c("sample", "threads"), sep="_")
+  return(bm_data)
+}
+
+plot_data <- function(data, output){
+  ggplot(data, aes(x=threads, y=s, color=tool)) +
+    facet_grid(~sample) +
+    geom_point()
+  
+  ggsave(snakemake@output[[1]])
+}
+
+# Run plotting workflow
+
+sink(file = snakemake@log[[1]])
+data <- get_data(snakemake@input)
+plot_data(data, snakemake@output[[1]])
